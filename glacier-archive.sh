1a3ae4a44a2c7fcb5a7060c13ea8d6d5f878dfe6
JiM Strassburg <jstrassburg@gmail.com>
2018-07-08 22:05:23 -0500

    Writing the upload-archive output to a log file

--- glacier-archive.sh
+++ glacier-archive.sh
@@ -13,23 +13,22 @@ CURRENT_DATE=$(date +%Y%m%d)
 GLACIER_VAULT=$1
 BACKUP_DIR=$2
 BACKUP_FILE=$BACKUP_DIR/$CURRENT_DATE-backup.tgz
-LAST_BACKUP=$BACKUP_DIR/last-backup
+BACKUP_LOG=$BACKUP_DIR/glacier-archive.log
 
-if [ ! -f $LAST_BACKUP ]; then
-  touch -d 19000101 $LAST_BACKUP
+if [ ! -f $BACKUP_LOG ]; then
+  touch -d 19000101 $BACKUP_LOG
 fi
 
 echo "Backing up new files in $BACKUP_DIR to $GLACIER_VAULT..."
 echo "Creating tarball $BACKUP_FILE..."
 
-find $BACKUP_DIR -newer $LAST_BACKUP -not -path $BACKUP_DIR | xargs tar cfvz $BACKUP_FILE
+find $BACKUP_DIR -newer $BACKUP_LOG -not -path $BACKUP_DIR | xargs tar cfvz $BACKUP_FILE
 
 if [ $? -eq 0 ]; then
     echo "Uploading $BACKUP_FILE to AWS Glacier..."
-    aws glacier upload-archive --account-id - --vault-name $GLACIER_VAULT --body $BACKUP_FILE
+    echo $CURRENT_DATE >> $BACKUP_LOG
+    aws glacier upload-archive --account-id - --vault-name $GLACIER_VAULT --body $BACKUP_FILE | tee -a $BACKUP_LOG
     rm -f $BACKUP_FILE
 else
     echo "No new files, skipping upload to AWS Glacier..."
 fi
-
-touch $LAST_BACKUP
